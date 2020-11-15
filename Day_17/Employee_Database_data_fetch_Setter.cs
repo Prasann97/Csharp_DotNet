e253c13d6761e144d1c87e8bcb42e4007492fb3f
Prasann97 <63595583+Prasann97@users.noreply.github.com>
2020-11-15 14:20:31 +0530

    Add files via upload

---
+++ Day_17/Employee_Database_data_fetch_Setter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApplication24
+{
+    class Program
+    {
+        public void details()
+        {
+            String connectionstring = @"Data Source=(localdb)\Projects;Initial Catalog=Demo;Integrated Security=True";
+            using (SqlConnection con = new SqlConnection())
+            {
+                con.ConnectionString = connectionstring;
+                SqlCommand cmd = new SqlCommand("Select * from Employee", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine("{0} {1} {2}", rdr["id"], rdr["Name"], rdr["Salary"]);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.details();
+        }
+    }
+}
